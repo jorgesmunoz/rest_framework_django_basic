4fa1bd5c6a9bc1337cba204c32e722c2aa3575ec
jorgesmunoz <jorgesmunoz@gmail.com>
2022-04-15 18:01:40 -0300

    added user serializer

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from profiles_api import serializers
 from rest_framework import status, viewsets
+from profiles_api import models
 
 
 # Create your views here.
@@ -89,3 +90,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Hanlde removing an object by its id"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+
+    # Queries the object
+    queryset = models.UserProfile.objects.all()
+
+    # This class has the same methods as ViewSet
